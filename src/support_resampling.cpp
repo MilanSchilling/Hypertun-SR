b6cb14a0628262e6f4f5b36d3bb7826b54748db4
Milaneseirokese <milansc@ethz.ch>
2017-03-28 13:48:28 +0200

    set up interface

--- src/support_resampling.cpp
+++ src/support_resampling.cpp
@@ -1,7 +1,17 @@
 #include "support_resampling.hpp"
 #include <iostream>
 
-void support_resampling(){
+void support_resampling(cv::Mat &C_g, cv::Mat &C_b, 
+                            cv::Mat &S_it, cv::Mat &S_it_next){
 
 	std::cout << "support_resampling.cpp" << std::endl;
+	// TODO: pass params as argument
+	int sz_occ = 32;
+
+	S_it_next = S_it;
+
+	// define vector of 2d points
+	std::vector<cv::Point2d> X;
+
+
 }
--- src/support_resampling.hpp
+++ src/support_resampling.hpp
@@ -1,5 +1,8 @@
 #ifndef SUPPORT_RESAMPLING_HPP
 #define SUPPORT_RESAMPLING_HPP
 
-void support_resampling();
+#include <opencv2/opencv.hpp>
+
+void support_resampling(cv::Mat &C_g, cv::Mat &C_b, 
+                            cv::Mat &S_it, cv::Mat &S_it_next);
 #endif // SUPPORT_RESAMPLING_HPP
