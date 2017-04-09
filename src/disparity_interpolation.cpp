5b48f1f790f384b615ca055e530e710c3faef590
Milaneseirokese <milansc@ethz.ch>
2017-04-09 18:45:10 +0200

    added description and deleted debug output

--- src/disparity_interpolation.cpp
+++ src/disparity_interpolation.cpp
@@ -1,37 +1,38 @@
 #include "disparity_interpolation.hpp"
 #include <iostream>
 
+
+// inputs:
+// - G   : HxW Matrix with an index for each pixel to the corresponding triangle
+// - T   : 4 x num_riangles matrix with the plane parameters for each pixel
+//
+// outputs:
+// - D_it: HxW matrix with an interpolated disparity for each pixel
 void disparity_interpolation(cv::Mat &G, cv::Mat &T, cv::Mat &D_it){
 	
 	std::cout << "disparity_interpolation.cpp" << std::endl;
 
+	// get image dimensions
 	int H = G.rows;
 	int W = G.cols;
-	cv::Mat D2 = cv::Mat(H, W, CV_32F, 0.0);
 
+	// loop over every pixel
 	for (int i = 0; i < H; ++i) {
 		for (int j = 0; j < W; ++j) {
+
+			// get index of the corresponding triangle 
 			int idx_trig = G.at<int>(i,j);
-			//std::cout << idx_trig << std::endl;
+
+			// get plane parameters
 			if (idx_trig >= 0) {
-				//std::cout << idx_trig << std::endl;
 				float a = T.at<float>(0,idx_trig);
 				float b = T.at<float>(1,idx_trig);
 				float c = T.at<float>(2,idx_trig);
 				float d = T.at<float>(3,idx_trig);
-				//std::cout << (d - a*i - b*j) / c << std::endl;
+
+				// interpolate disparity
 				D_it.at<float>(i,j) = (d - a*j - b*i) / c;
-				D2.at<float>(i,j) = (d - a*j - b*i) / c;
 			}
 		}
-	}
-
-	//std::cout << D_it.at<float>(1,1) << std::endl;
-	cv::Mat dst;
-	D2.convertTo(dst, CV_8U, 0.5);
-	//cv::normalize(dst, dst, 0, 1, cv::NORM_MINMAX);
-
-	cv::imshow("Interpolated Disparity", dst);
-	cv::waitKey(0);
-	
+	}	
 }
